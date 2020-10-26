03ce6b1970d776994b4804bc04593d404a4df565
Glitch (adrianreilly-portfolio) <none>
2020-10-26 18:13:38 +0000

    💨☄️ Updated with Glitch

---
+++ script.js
@@ -0,0 +1,95 @@
+$(".home").mouseenter(function() {
+  $(".home").css("color", "black");
+});
+
+$(".home").mouseleave(function() {
+  $(".home").css("color", "silver");
+});
+
+$(".aboutMe").mouseenter(function() {
+  $(".aboutMe").css("color", "black");
+});
+
+$(".aboutMe").mouseleave(function() {
+  $(".aboutMe").css("color", "silver");
+});
+
+$(".projects").mouseenter(function() {
+  $(".projects").css("color", "black");
+});
+
+$(".projects").mouseleave(function() {
+  $(".projects").css("color", "silver");
+});
+
+$(".contact").mouseenter(function() {
+  $(".contact").css("color", "black");
+});
+
+$(".contact").mouseleave(function() {
+  $(".contact").css("color", "silver");
+});
+
+$(".box1").mouseenter(function() {
+  $(".box1").css("height", "280px");
+  $(".box1").css("width", "380");
+  $(".box1").css("background-color", "white");
+});
+
+$(".box1").mouseleave(function() {
+  $(".box1").css("height", "250");
+  $(".box1").css("width", "350");
+  $(".box1").css("background-color", "silver");
+});
+
+$(".box2").mouseenter(function() {
+  $(".box2").css("height", "280px");
+  $(".box2").css("width", "380");
+  $(".box2").css("background-color", "white");
+});
+
+$(".box2").mouseleave(function() {
+  $(".box2").css("height", "250");
+  $(".box2").css("width", "350");
+  $(".box2").css("background-color", "silver");
+});
+
+$(".box3").mouseenter(function() {
+  $(".box3").css("height", "280px");
+  $(".box3").css("width", "380");
+  $(".box3").css("background-color", "white");
+});
+
+$(".box3").mouseleave(function() {
+  $(".box3").css("height", "250");
+  $(".box3").css("width", "350");
+  $(".box3").css("background-color", "silver");
+});
+
+$(".box4").mouseenter(function() {
+  $(".box4").css("height", "280px");
+  $(".box4").css("width", "380");
+  $(".box4").css("background-color", "white");
+});
+
+$(".box4").mouseleave(function() {
+  $(".box4").css("height", "250");
+  $(".box4").css("width", "350");
+  $(".box4").css("background-color", "silver");
+});
+
+$(".homeBtn").click(function() {
+  window.location.href = "index.html";
+});
+
+$(".aboutMeBtn").click(function() {
+  window.location.href = "aboutMe.html";
+});
+
+$(".projectsBtn").click(function() {
+  window.location.href = "projects.html";
+});
+
+$(".contactBtn").click(function() {
+  window.location.href = "contact.html";
+});
